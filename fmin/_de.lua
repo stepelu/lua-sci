429ccd3908f3d1e17f2ccaa7406b356a09f3555f
Stefano Peluchetti <phd.st.p@gmail.com>
2014-07-24 16:09:58 +0900

    FIX: diff.de randtobest_2_bin (faster), update to new sci.alg, handle non-linear constraints, chances to interface (simpler)

--- fmin/_de.lua
+++ fmin/_de.lua
@@ -10,8 +10,8 @@
 --------------------------------------------------------------------------------
 
 -- Here implemented is the differential evolution algorithm presented in the 
--- paper: "Differential evolution algorithm with strategy adaptation for global 
--- numerical optimization", 2009.
+-- paper: "Self-adaptive Differential Evolution Algorithm for Constrained 
+-- Real-Parameter Optimization", 2006.
 -- The following modifications have been performed: 
 -- + in the paper a moving window of LP generations is used to update the 
 --   parameters; we simply re-update the parameters every LP generations using 
@@ -34,29 +34,39 @@ local min, max, abs, floor, ceil, step, sqrt = xsys.from(math,
      "min, max, abs, floor, ceil, step, sqrt")
      
 local normald = dist.normal
-
 local alg32 = alg.typeof("int32_t")
-local row = alg.row
 
 local function rand_1_bin(v, j, rj, xmin, x, F, K)
   local j1, j2, j3 = rj[j][1], rj[j][2], rj[j][3]
-  v:set(x[j1] + (x[j2] - x[j3])*F[j])
+  local x1, x2, x3, F0 = x[j1], x[j2], x[j3], F[j]
+  for i=1,#v do 
+    v[i] = x1[i] + (x2[i] - x3[i])*F0 
+  end
 end
 
 local function rand_2_bin(v, j, rj, xmin, x, F, K)
   local j1, j2, j3, j4, j5 = rj[j][1], rj[j][2], rj[j][3], rj[j][4], rj[j][5]
-  v:set(x[j1] + (x[j2] - x[j3])*F[j] + (x[j4] - x[j5])*F[j])
+  local x1, x2, x3, x4, x5, F0 = x[j1], x[j2], x[j3], x[j4], x[j5], F[j]
+  for i=1,#v do 
+    v[i] = x1[i] + (x2[i] - x3[i])*F0 + (x4[i] - x5[i])*F0 
+  end
 end
 
 local function randtobest_2_bin(v, j, rj, xmin, x, F, K)
   local j1, j2, j3, j4 = rj[j][1], rj[j][2], rj[j][3], rj[j][4]
-  v:set(x[j] + (xmin - x[j])*F[j] + (x[j1] - x[j2])*F[j] 
-                                  + (x[j3] - x[j4])*F[j])
+  local x0, x1, x2, x3, x4, F0 = x[j], x[j1], x[j2], x[j3], x[j4], F[j]
+  for i=1,#v do 
+    v[i] = x0[i] + (xmin[i] - x0[i])*F0 + (x1[i] - x2[i])*F0 
+                                        + (x3[i] - x4[i])*F0 
+  end
 end
 
 local function currenttorand_1(v, j, rj, xmin, x, F, K)
   local j1, j2, j3 = rj[j][1], rj[j][2], rj[j][3]
-  v:set(x[j] + (x[j1] - x[j])*K[j] + (x[j2] - x[j3])*F[j])
+  local x0, x1, x2, x3, F0, K0 = x[j], x[j1], x[j2], x[j3], F[j], K[j]
+  for i=1,#v do 
+    v[i] = x0[i] + (x1[i] - x0[i])*K0 + (x2[i] - x3[i])*F0 
+  end
 end
 
 local strategies = { 
@@ -68,7 +78,6 @@ local strategies = {
 
 -- Branch-free sampling of strategy according to the four probabilities in p.
 local function sample_strategy_indices(rng, rs, p)
-  assert(#p == #strategies)
   for j=1,#rs do
     local u = rng:sample()
     rs[j] = 1 
@@ -102,12 +111,16 @@ local function sample_distinct_indices(rng, rj)
   end
 end
 
-local function sample_F(rng, F, Fmu, Fsigma)
-  for i=1,#F do F[i] = rng:sample() end
+local function sample_F(rng, F)
+  for i=1,#F do 
+    F[i] = rng:sample()
+  end
 end
 
 local function sample_K(rng, K)
-  for i=1,#K do K[i] = rng:sample() end
+  for i=1,#K do 
+    K[i] = rng:sample() 
+  end
 end
 
 -- MODIFICATION: Our algorithm performs flooring at 0 and 1.
@@ -119,12 +132,14 @@ local function sample_CR(rng, CR, CRmu, CRsigma)
   end
 end
 
--- Element equal to one means 
+-- Element equal to one means mutation.
 local function sample_mutations(rng, rz, rs, CR)
   local NP, dim = #rz, #rz[1]
   for j=1,NP do
     if rs[j] == 4 then
-      rz[j]:fill(1)
+      for d=1,dim do
+        rz[j][d] = 1
+      end
     else
       for d=1,dim do
         rz[j][d] = step(CR[j] - rng:sample())
@@ -135,14 +150,29 @@ local function sample_mutations(rng, rz, rs, CR)
   end
 end
 
-local function updatemin(xmin, fmin, xval, fval)
-  if fval ~= fval then
-    error("nan value encountered, check objective function definition")
-  end
-  if fval < fmin then
-    return xval, fval
+local function nan_to_inf(x)
+  return x == x and x or 1/0
+end
+
+-- Notice that to avoid stagnation we favor f1 and v1 over f2 and v2.
+local function compare(scale, f1, v1, f2, v2)
+  local vmin, vmax = min(v1, v2), max(v1, v2)
+  if vmax == 0 then -- Both satisfy the constraints.    
+    return nan_to_inf(scale*f1) <= nan_to_inf(scale*f2)
+  elseif vmin > 0 then -- Neither satisfy the constraints.
+    return v1 <= v2
   else
-    return xmin, fmin
+    return v1 == 0 -- Who satisfy the constraint wins, and one of them do.
+  end
+end
+
+local function updatemin(scale, xmin, fmin, vmin, xnew, fnew, vnew)
+  if compare(scale, fnew, vnew, fmin, vmin) then
+    -- Note copy is made only when new particle is a new global minimum. 
+    -- But the copy NEEDS to be made.
+    return xnew:copy(), fnew, vnew
+  else
+    return xmin, fmin, vmin
   end
 end
 
@@ -154,15 +184,26 @@ local function stop_iter(n)
   end
 end
 
+local function no_violation(x)
+  return 0
+end
+
 local function rows_as_vec(x)
   local o =  {}
   for i=1,x:nrow() do
-    o[i] = x:copy(row(i))
+    o[i] = alg.vec(x:ncol())
+    for j=1,x:ncol() do
+      o[i][j] = x[i][j]
+    end
   end
   return o
 end
 
-local function fmin(f, o)
+local function optim(scale, f, o)
+  local violation = o.violation or no_violation
+  local rng       = o.rng       or prng.std()
+  local bound     = o.bound     or "reflect"
+
   local stop = o.stop
   if type(stop) == "number" then
     if stop < 1 then
@@ -170,59 +211,53 @@ local function fmin(f, o)
     end
     stop = stop_iter(stop)
   end
-    
-  local rng = o.rng or prng.std()
-  local bound = o.bound or "reflect"
-  local LP = o.LP or 20
   
-  local dim = o.xval and o.xval:ncol() or #o.xl
-  local NP = o.NP or min(10, 8*dim)
+  local xl, xu = o.xl, o.xu
+  if xl then
+    if #xl ~= #xu then
+      error("xl and xu must have the same size")
+    end
+    for i=1,#xl do
+      if not (xl[i] < xu[i]) then
+        error("xl < xu required")
+      end
+    end
+  end
+
+  local NP, dim, xval, x -- Current population.
+  if o.x0 then
+    xval = o.x0:copy()
+    NP, dim = xval:nrow(), xval:ncol()
+    x = rows_as_vec(xval)
+  else
+    dim = #xl
+    NP = o.np or max(10, 8*dim)
+    xval = alg.mat(NP, dim)
+    x = rows_as_vec(xval)
+    local popd = dist.mvuniform(xl, xu)
+    for j=1,NP do
+      popd:sample(rng, x[j])
+      for i=1,dim do
+        xval[j][i] = x[j][i] 
+      end
+    end
+  end
   if NP < 10 then
     error("NP >= 10 required")
   end
- 
-  -- Can be present or not, depending on whether a new population is randomly
-  -- generated or not and whether bounds are applied or not:
-  local xl, xu = o.xl, o.xu
-  if xl and xu then
-    if not (xl < xu) then
-      error("xl < xu required")
-    end
-  end
-  
-  local xval, fval, x
-  local xmin, fmin = nil, 1/0 -- Reference to minimum xval.
-  if o.xval and o.fval then -- Initialize with argument population.
-    xval = alg.mat(o.xval)
-    fval = alg.vec(o.fval)
-    x = rows_as_vec(xval)
-    for j=1,NP do
-      xmin, fmin = updatemin(xmin, fmin, x[j], fval[j])
-    end
-  else -- Initialize with randomly generated population.
-    xval = alg.mat(NP, dim)
-    fval = alg.vec(NP)
-    x = rows_as_vec(xval)
-    local maxinit = o.maxinit or 100*NP -- At least 1% in support zone.
-    local iter = 0
-    local popd = dist.mvuniform(xl, xu)
-    for j=1,NP do
-      repeat
-        iter = iter + 1
-        if iter > maxinit then
-          return nil, "failed to initialize population, too many infinite "
-                    .."evaluation of f, try to increase maxinit"
-        end
-        popd:sample(rng, x[j])
-        fval[j] = f(x[j])
-      until fval[j] < 1/0
-      xmin, fmin = updatemin(xmin, fmin, x[j], fval[j])
-    end
+
+  local fval, vval = alg.vec(NP), alg.vec(NP)
+  local xmin, fmin, vmin = nil, 1/0, 1/0
+  for j=1,NP do
+    vval[j] = violation(x[j])
+    fval[j] = vval[j] == 0 and f(x[j]) or 0/0
+    xmin, fmin, vmin = updatemin(scale, xmin, fmin, vmin, x[j], fval[j], 
+      vval[j])
   end
      
   -- Equal probability to each strategy:
   local Pmu = alg.vec(4, 1/4)
-  -- Centred around 0.5 with good dispersion: 68.2% of mass in (0, 1).
+  -- Centered around 0.5 with good dispersion: 68.2% of mass in (0, 1).
   local CRmu, CRsigma = 0.5, 0.5
   
   local Pstat = { } 
@@ -231,32 +266,32 @@ local function fmin(f, o)
   local CRstat = stat.olvar(0)
   local nsuccess = 0
 
-  local F = alg.vec(NP)
-  local K = alg.vec(NP)
+  local F  = alg.vec(NP)
+  local K  = alg.vec(NP)
   local CR = alg.vec(NP)
-  
-  local rs = alg32.vec(NP)      -- Random strategies.
-  local rj = alg32.mat(NP, 5)   -- Random j-indices.
-  
+  local rs = alg32.vec(NP)    -- Random strategies.
+  local rj = alg32.mat(NP, 5) -- Random j-indices.
   local rz = { } -- Dimensions which mutate.
-  local u = { } -- Mutated particles.
-  for i=1,NP do
-    rz[i], u[i] = alg.vec(dim), alg.vec(dim)
+  local v  = alg.vec(dim) -- Potential mutation particle.
+  local u  = { } -- Mutated particles.
+  for j=1,NP do
+    rz[j], u[j] = alg.vec(dim), alg.vec(dim)
   end
-  local v = alg.vec(dim)        -- Potential mutation particle.
       
   local generation = 0
-  -- io.write("GENE SUCC CRMU   CRSI   P1     P2     P3     P4\n")
-  while not stop(xmin, fmin, xval, fval) do
+  while not stop(xmin, fmin, vmin, xval, fval, vval) do
     generation = generation + 1
     -- Update meta-parameters.
-    if generation % LP == 0 and nsuccess >= 100 then      
+    if generation % 20 == 0 and nsuccess >= 100 then      
       for i=1,4 do
         Pmu[i] = Pstat[i]:mean() + 0.01
         Pstat[i]:clear()
       end
-      Pmu:set(Pmu/Pmu:sum()) 
-      CRmu, CRsigma = CRstat:mean(), max(0.1, sqrt(CRstat:var()))
+      local sum = 0 
+      for i=1,4 do sum = sum + Pmu[i] end
+      for i=1,4 do Pmu[i] = Pmu[i]/sum end
+      CRmu = max(0, min(CRmu + 2*(CRstat:mean() - CRmu), 1))
+      CRsigma = max(0.1, sqrt(CRstat:var()))
       CRstat:clear()
       nsuccess = 0
     end
@@ -272,7 +307,9 @@ local function fmin(f, o)
       -- Potential mutation:
       strategies[rs[j]](v, j, rj, xmin, x, F, K)
       -- Mutated particle:
-      u[j]:set(rz[j]*v + (1 - rz[j])*x[j])
+      for i=1,dim do 
+        u[j][i] = rz[j][i]*v[i] + (1 - rz[j][i])*x[j][i] 
+      end
       -- Apply the bounds:
       for i=1,dim do
         if bound == "reflect" then        
@@ -291,25 +328,31 @@ local function fmin(f, o)
     -- Selection:
     for j=1,NP do
       local s = rs[j]
-      local fuj = f(u[j])
-      if fuj < fval[j] then -- It's an improvement --> select.
+      local vuj = violation(u[j])
+      local fuj = vuj == 0 and f(u[j]) or 0/0
+      if compare(scale, fuj, vuj, fval[j], vval[j]) then
+        -- It's an improvement --> select.
         nsuccess = nsuccess + 1
         Pstat[s]:push(1)
         CRstat:push(CR[j])
-        fval[j] = fuj
-        x[j]:set(u[j])
-        xmin, fmin = updatemin(xmin, fmin, x[j], fuj)
+        for i=1,dim do 
+          x[j][i] = u[j][i] 
+        end
+        fval[j], vval[j] = fuj, vuj        
+        xmin, fmin, vmin = updatemin(scale, xmin, fmin, vmin, u[j], fuj, vuj)
       else
         Pstat[s]:push(0)
       end
       -- Update xval for next generation (used only in stop()):
-      xval:set(x[j], row(j))
+      for i=1,dim do 
+        xval[j][i] = x[j][i] 
+      end
     end
   end
   
-  return xmin:copy(), fmin, xval, fval
+  return xmin:copy(), fmin, vmin, xval, fval:copy(), vval:copy()
 end
 
 return { 
-  fmin = fmin,
+  optim = optim,
 }
