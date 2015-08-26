366bbcd01c47014ee3efec01cca4bc56c4380aeb
Stefano Peluchetti <phd.st.p@gmail.com>
2015-08-26 19:27:40 +0100

    Add __meta.lua.

---
+++ __meta.lua
@@ -0,0 +1,10 @@
+return {
+  version = "1.0.beta7",
+  require = {
+    luajit = "2.0",
+    xsys   = "1.0",
+  },
+  license = "MIT <http://opensource.org/licenses/MIT>",
+  homepage = "http://scilua.org/sci.html",
+  description = "SCI - General Purpose Scientific Library",
+}
