aadb5c121c42bd4f48737ce4ec9f1abe19964f48
Stefano Peluchetti <phd.st.p@gmail.com>
2015-09-12 20:07:38 +0100

    Prepare for 1.0-beta8.

--- __meta.lua
+++ __meta.lua
@@ -1,10 +1,11 @@
 return {
-  version = "1.0.beta7",
+  name = "sci",
+  version = "1.0.beta8",
   require = {
     luajit = "2.0",
     xsys   = "1.0",
   },
   license = "MIT <http://opensource.org/licenses/MIT>",
   homepage = "http://scilua.org/sci.html",
-  description = "SCI - General Purpose Scientific Library",
+  description = "general purpose scientific computing library",
 }
