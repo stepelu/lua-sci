14cd18f5dc3b87766523d819e4ba43479551b6bb
Stefano Peluchetti <phd.st.p@gmail.com>
2015-11-03 18:45:04 +0000

    Prepare for v1.0.0-beta9

--- __meta.lua
+++ __meta.lua
@@ -1,9 +1,10 @@
 return {
   name = "sci",
-  version = "1.0.beta8",
+  version = "1.0.beta9",
   require = {
     luajit = "2.0",
     xsys   = "1.0",
+    clib_libopenblas = "0.2.15",
   },
   license = "MIT <http://opensource.org/licenses/MIT>",
   homepage = "http://scilua.org/sci.html",
